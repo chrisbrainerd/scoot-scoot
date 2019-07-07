278f1a27e647b5224c53a48367b4a9e43a83de69
chrisbrainerd <christopher.brainerd@gmail.com>
2019-07-06 22:17:21 -0400

    filters! ��

--- scoot-scoot-ui/src/Map/Filters.jsx
+++ scoot-scoot-ui/src/Map/Filters.jsx
@@ -1,15 +1,226 @@
-import React, { useState } from 'react';
-import Fab from '@material-ui/core/Fab';
-import FilterList from '@material-ui/icons/FilterList';
+import React, { useState } from "react";
+import Fab from "@material-ui/core/Fab";
+import FilterList from "@material-ui/icons/FilterList";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import Switch from "@material-ui/core/Switch";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
 
-const Filters = props => {
+import BirdLogo from "./Bird2.png";
+import BikeshareLogo from "./Bikeshare.png";
+import JumpLogo from "./Jump.jpg";
+import LyftLogo from "./lyft.png";
+import SpinLogo from "./spin.png";
+
+const Filters = ({ updateFilters }) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const [birdScooters, setBirdScooters] = useState(true);
+  const [bikeshares, setBikeshares] = useState(true);
+  const [jumpBikes, setJumpBikes] = useState(true);
+  const [lyftScooters, setLyftScooters] = useState(true);
+  const [spinScooters, setSpinScooters] = useState(true);
+
+  const [filterByBattery, setFilterByBattery] = useState(false);
+  const [minBattery, setMinBattery] = useState(0);
+
+  const closeAndUpdateHost = () => {
+    updateFilters({
+      showBirdScooters: birdScooters,
+      showBikeshares: bikeshares,
+      showJumpBikes: jumpBikes,
+      showLyftScooters: lyftScooters,
+      showSpinScooters: spinScooters,
+      filterByBattery: filterByBattery,
+      minBattery: minBattery
+    });
+    setDrawerIsOpen(false);
+  };
+
+  const activeFilterCount = // lol production code
+    6 -
+    +birdScooters -
+    +bikeshares -
+    +jumpBikes -
+    +lyftScooters -
+    +spinScooters -
+    +!filterByBattery;
 
   return (
-    <div id='fab'>
-      <Fab color="primary" aria-label="Add" >
+    <div id="fab">
+      <Fab
+        onClick={() => {
+          setDrawerIsOpen(true);
+        }}
+        color="primary"
+        aria-label="Add"
+      >
         <FilterList />
+        {!!activeFilterCount && <span id="fab-number" />}
       </Fab>
+      <Drawer anchor="right" open={drawerIsOpen} onClose={closeAndUpdateHost}>
+        <List
+          className="filter-drawer"
+          subheader={<ListSubheader>Filters</ListSubheader>}
+        >
+          <ListItem onClick={() => setBirdScooters(!birdScooters)}>
+            <ListItemText id="switch-list-label-bird" className="filter-item">
+              <img
+                className="filter-logo"
+                src={BirdLogo}
+                alt="Bird Scooters Logo"
+              />
+              Bird
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <Switch
+                edge="end"
+                onChange={() => setBirdScooters(!birdScooters)}
+                checked={birdScooters}
+                inputProps={{ "aria-labelledby": "switch-list-label-bird" }}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+
+          <ListItem onClick={() => setBikeshares(!bikeshares)}>
+            <ListItemText
+              id="switch-list-label-bikeshares"
+              className="filter-item"
+            >
+              <img
+                className="filter-logo"
+                src={BikeshareLogo}
+                alt="Capital Bikeshare Logo"
+              />
+              Capital Bikeshare
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <Switch
+                edge="end"
+                onChange={() => setBikeshares(!bikeshares)}
+                checked={bikeshares}
+                inputProps={{
+                  "aria-labelledby": "switch-list-label-bikeshares"
+                }}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+
+          <ListItem onClick={() => setJumpBikes(!jumpBikes)}>
+            <ListItemText id="switch-list-label-jump" className="filter-item">
+              <img className="filter-logo" src={JumpLogo} alt="Jump Logo" />
+              JUMP e-bikes
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <Switch
+                edge="end"
+                onChange={() => setJumpBikes(!jumpBikes)}
+                checked={jumpBikes}
+                inputProps={{
+                  "aria-labelledby": "switch-list-label-jump"
+                }}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+
+          <ListItem onClick={() => setLyftScooters(!lyftScooters)}>
+            <ListItemText id="switch-list-label-lyft" className="filter-item">
+              <img className="filter-logo" src={LyftLogo} alt="Lyft Logo" />
+              Lyft scooters
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <Switch
+                edge="end"
+                onChange={() => setLyftScooters(!lyftScooters)}
+                checked={lyftScooters}
+                inputProps={{
+                  "aria-labelledby": "switch-list-label-lyft"
+                }}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+
+          <ListItem onClick={() => setSpinScooters(!spinScooters)}>
+            <ListItemText id="switch-list-label-spin" className="filter-item">
+              <img className="filter-logo" src={SpinLogo} alt="Spin Logo" />
+              Spin scooters
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <Switch
+                edge="end"
+                onChange={() => setSpinScooters(!spinScooters)}
+                checked={spinScooters}
+                inputProps={{
+                  "aria-labelledby": "switch-list-label-spin"
+                }}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+          <Divider />
+
+          <ListItem onClick={() => setFilterByBattery(!filterByBattery)}>
+            <ListItemText
+              id="switch-list-label-filterByBattery"
+              className="filter-item"
+            >
+              Filter by battery charge
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <Switch
+                edge="end"
+                onChange={() => setFilterByBattery(!filterByBattery)}
+                checked={filterByBattery}
+                inputProps={{
+                  "aria-labelledby": "switch-list-label-filterByBattery"
+                }}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+          {filterByBattery && (
+            <ListItem>
+              <Input
+                id="charge-percent"
+                value={minBattery}
+                onChange={e => setMinBattery(+e.target.value)}
+                endAdornment={<InputAdornment position="end">%</InputAdornment>}
+                type="number"
+                inputProps={{
+                  "aria-label": "minimum charge percentage",
+                  min: "0",
+                  max: "100"
+                }}
+              />
+            </ListItem>
+          )}
+        </List>
+        <div className="drawer-buttons">
+          <Divider />
+          <Button
+            variant="contained"
+            onClick={() => {
+              setBirdScooters(true);
+              setBikeshares(true);
+              setJumpBikes(true);
+              setLyftScooters(true);
+              setSpinScooters(true);
+              setFilterByBattery(false);
+            }}
+          >
+            Reset all
+          </Button>
+          <Button variant="contained" onClick={closeAndUpdateHost}>
+            Apply filters
+          </Button>
+        </div>
+      </Drawer>
     </div>
-  )
-}
+  );
+};
 export default Filters;
--- scoot-scoot-ui/src/Map/Map.jsx
+++ scoot-scoot-ui/src/Map/Map.jsx
@@ -1,21 +1,27 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-import MarkerClusterGroup from 'react-leaflet-markercluster';
-import io from 'socket.io-client';
+import React, { useRef, useState, useEffect } from "react";
+import {
+  Map,
+  Marker,
+  Popup,
+  TileLayer,
+  AttributionControl
+} from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
+import io from "socket.io-client";
 
 import { geolocated } from "react-geolocated";
-import L from 'leaflet';
-import useRect from './useRect';
-import useLocation from './useLocation';
-import BirdLogo from './Bird2.png';
-import BikeshareLogo from './Bikeshare.png';
-import JumpLogo from './Jump.jpg';
-import LyftLogo from './lyft.png';
-import SpinLogo from './spin.png';
+import L from "leaflet";
+import useRect from "./useRect";
+import useLocation from "./useLocation";
+import BirdLogo from "./Bird2.png";
+import BikeshareLogo from "./Bikeshare.png";
+import JumpLogo from "./Jump.jpg";
+import LyftLogo from "./lyft.png";
+import SpinLogo from "./spin.png";
 
-import Filters from './Filters';
+import Filters from "./Filters";
 
-require('./Map.less');
+require("./Map.less");
 
 let mySvgString = `
 <svg  height="20" width="20" xmlns="http://www.w3.org/2000/svg">
@@ -27,127 +33,206 @@ let mySvgString = `
   </defs>
   <circle class='pulse-me' cx="10" cy="10" r="10" stroke-width="3" fill="url('#myGradient')"  />
 </svg>
-`
+`;
 
-let myIconUrl = encodeURI("data:image/svg+xml," + mySvgString).replace('#','%23');
+let myIconUrl = encodeURI("data:image/svg+xml," + mySvgString).replace(
+  "#",
+  "%23"
+);
 
 const YouIcon = new L.icon({
   iconUrl: myIconUrl,
   iconSize: 20,
-  className: 'pulse-me'
+  className: "pulse-me"
 });
 const BirdIcon = new L.Icon({
   iconUrl: BirdLogo,
   iconSize: 30
-})
+});
 const BikeshareIcon = new L.Icon({
   iconUrl: BikeshareLogo,
   iconSize: 30
-})
+});
 const JumpIcon = new L.Icon({
   iconUrl: JumpLogo,
   iconSize: 30
-})
+});
 const LyftIcon = new L.Icon({
   iconUrl: LyftLogo,
   iconSize: 30
-})
+});
 const SpinIcon = new L.Icon({
   iconUrl: SpinLogo,
   iconSize: 30
-})
+});
 
-// const protocol = document.location.protocol === "https:" ? "wss:" : "ws:"
+const endpoint =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://" + document.domain;
 
-// const endpoint = 'https://' + document.domain + ':3003';
-const endpoint = 'https://' + document.domain;
-
-const MapComponent = ({
-  coords
-}) => {
+const MapComponent = ({ coords }) => {
   const wrapperRef = useRef(null);
   const rect = useRect(wrapperRef);
   const location = useLocation(coords);
 
-  const [birdScooters, setBirdScooters] = useState([])
-  const [bikeshares, setBikeshares] = useState([]);
-  const [jumpBikes, setJumpBikes] = useState([]);
-  const [lyftScooters, setLyftScooters] = useState([]);
-  const [spinScooters, setSpinScooters] = useState([]);
+  const [corpus, setCorpus] = useState({
+    birdScooters: [],
+    bikeshares: [],
+    jumpBikes: [],
+    lyftScooters: [],
+    spinScooters: []
+  });
+
+  const [filters, setFilters] = useState({
+    showBirdScooters: true,
+    showBikeshares: true,
+    showJumpBikes: true,
+    showLyftScooters: true,
+    showSpinScooters: true,
+    filterByBattery: false,
+    minBattery: 0
+  });
 
   useEffect(() => {
     const socket = io(endpoint);
     socket.on("data-update", corpus => {
-      console.log('received data update!', corpus);
-        setBirdScooters(corpus.birdScooters);
-        setBikeshares(corpus.bikeshares);
-        setJumpBikes(corpus.jumpBikes);
-        setLyftScooters(corpus.lyftScooters);
-        setSpinScooters(corpus.spinScooters);
-    })
+      // console.log("received data update!", corpus);
+      setCorpus(corpus);
+    });
 
     return function cleanup() {
       socket.close();
-    }
-  }, [])
+    };
+  }, []);
 
+  const {
+    birdScooters,
+    bikeshares,
+    jumpBikes,
+    lyftScooters,
+    spinScooters
+  } = corpus;
+
+  const {
+    filterByBattery,
+    minBattery,
+    showBirdScooters,
+    showBikeshares,
+    showJumpBikes,
+    showLyftScooters,
+    showSpinScooters
+  } = filters;
   return (
     <div className="map-wrapper" ref={wrapperRef}>
-      <Filters />
-      {rect.height && 
-        <Map 
-          center={location} 
-          zoom={13} 
-          style={{height: `${rect.height}px`, width: `${rect.width}px`}}
+      <Filters updateFilters={setFilters} />
+      {rect.height && (
+        <Map
+          center={location}
+          zoom={13}
+          style={{ height: `${rect.height}px`, width: `${rect.width}px` }}
           maxZoom={18}
+          attributionControl={false}
         >
+          <AttributionControl position="bottomright" prefix={false} />
           <TileLayer
             url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+            attribution="©2019 <a href='mailto:christopher.brainerd@gmail.com?subject=dcscootermap'>Chris Brainerd</a>"
           />
           <Marker position={location} icon={YouIcon}>
-            <Popup>It you</Popup>
+            <Popup>
+              It you
+              <span role="img" aria-label="Dancing emoji">
+                💃
+              </span>
+            </Popup>
           </Marker>
           <MarkerClusterGroup>
-            {birdScooters.map(scooter => (
-              <Marker key={scooter.id} position={[scooter.lat, scooter.lng]} icon={BirdIcon}>
-                <Popup>Battery: {scooter.battery}</Popup>
-              </Marker>
-            ))}
-            {bikeshares.map(bikeStation => (
-              <Marker key={bikeStation.station_id} position={[bikeStation.lat, bikeStation.lon]} icon={BikeshareIcon}>
-                <Popup>
-                  <p>{bikeStation.name}</p>
-                  <p>{bikeStation.num_bikes_available} bikes available{bikeStation.num_bikes_available === 0 && " 😭"}</p>
-                  <p>{bikeStation.num_ebikes_available} e-bikes available{bikeStation.num_bikes_available === 0 && " 😭"}</p>
-                  <p><a href={bikeStation.rental_url}>Rent now</a></p>
-                </Popup>
-              </Marker>
-            ))}
-            {jumpBikes.map(bike => (
-              <Marker key={bike.id} position={[bike.lat, bike.lng]} icon={JumpIcon}>
-                <Popup>Battery: {bike.battery}</Popup>
-              </Marker>
-            ))}
-            {lyftScooters.map(scooter => (
-              <Marker key={scooter.id} position={[scooter.lat, scooter.lng]} icon={LyftIcon}>
-                <Popup>Battery: {scooter.battery}</Popup>
-              </Marker>
-            ))}
-            {spinScooters.map(scooter => (
-              <Marker key={scooter.id} position={[scooter.lat, scooter.lng]} icon={SpinIcon}>
-                <Popup>Battery: {scooter.battery}</Popup>
-              </Marker>
-            ))}
+            {showBirdScooters &&
+              birdScooters.map(
+                scooter =>
+                  (!filterByBattery || scooter.battery > minBattery) && (
+                    <Marker
+                      key={scooter.id}
+                      position={[scooter.lat, scooter.lng]}
+                      icon={BirdIcon}
+                    >
+                      <Popup>Battery: {scooter.battery}</Popup>
+                    </Marker>
+                  )
+              )}
+            {showBikeshares &&
+              bikeshares.map(bikeStation => (
+                <Marker
+                  key={bikeStation.station_id}
+                  position={[bikeStation.lat, bikeStation.lon]}
+                  icon={BikeshareIcon}
+                >
+                  <Popup>
+                    <p>{bikeStation.name}</p>
+                    <p>
+                      {bikeStation.num_bikes_available} bikes available
+                      {bikeStation.num_bikes_available === 0 && " 😭"}
+                    </p>
+                    <p>
+                      {bikeStation.num_ebikes_available} e-bikes available
+                      {bikeStation.num_bikes_available === 0 && " 😭"}
+                    </p>
+                    <p>
+                      <a href={bikeStation.rental_url}>Rent now</a>
+                    </p>
+                  </Popup>
+                </Marker>
+              ))}
+            {showJumpBikes &&
+              jumpBikes.map(
+                bike =>
+                  (!filterByBattery || bike.battery > minBattery) && (
+                    <Marker
+                      key={bike.id}
+                      position={[bike.lat, bike.lng]}
+                      icon={JumpIcon}
+                    >
+                      <Popup>Battery: {bike.battery}</Popup>
+                    </Marker>
+                  )
+              )}
+            {showLyftScooters &&
+              lyftScooters.map(
+                scooter =>
+                  (!filterByBattery || scooter.battery > minBattery) && (
+                    <Marker
+                      key={scooter.id}
+                      position={[scooter.lat, scooter.lng]}
+                      icon={LyftIcon}
+                    >
+                      <Popup>Battery: {scooter.battery}</Popup>
+                    </Marker>
+                  )
+              )}
+            {showSpinScooters &&
+              spinScooters.map(
+                scooter =>
+                  (!filterByBattery || scooter.battery > minBattery) && (
+                    <Marker
+                      key={scooter.id}
+                      position={[scooter.lat, scooter.lng]}
+                      icon={SpinIcon}
+                    >
+                      <Popup>Battery: {scooter.battery}</Popup>
+                    </Marker>
+                  )
+              )}
           </MarkerClusterGroup>
         </Map>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default geolocated({
   positionOptions: {
     enableHighAccuracy: true
   },
-  // watchPosition: true
+  watchPosition: true
 })(MapComponent);
